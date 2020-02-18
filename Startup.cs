22363f77a8d37c034a8c43e446c939e4ca67fdda
Sebastian Idziak <i.basti@wp.com>
2020-02-18 21:53:40 +0100

    Added newtonsoft json support

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace SSSApp.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddDbContext<DataContext>(c => c.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
