490f652d833003103a4c6197f310e62ef7a74cfc
Sebastian Idziak <i.basti@wp.com>
2020-02-23 21:31:41 +0100

    Created driver controller with simple gets

---
+++ Controllers/DriverController.cs
@@ -0,0 +1,33 @@
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SSSApp.API.Data;
+
+namespace SSSApp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DriverController : ControllerBase
+    {
+        private readonly DataContext dataContext;
+
+        public DriverController(DataContext dataContext)
+        {
+            this.dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetDrivers()
+        {
+            var drivers = await dataContext.Driver.ToListAsync();
+            return Ok(drivers);
+        }
+
+        [HttpGet("{driverId}")]
+        public async Task<IActionResult> GetDrivers(int driverId)
+        {
+            var driver = await dataContext.Driver.FirstOrDefaultAsync(d => d.DriverId == driverId);
+            return Ok(driver);
+        }
+    }
+}
